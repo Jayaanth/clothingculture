3909c7823dfe2c7750f64768cce5a0537c7373a2
R JAYANTH JADHAV <77871395+Jayaanth@users.noreply.github.com>
2026-06-08 14:21:01 +0000

    db mount

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./clothingculture.db"
+DATABASE_URL = "sqlite:////var/data/clothingculture.db"
 
 engine = create_engine(
     DATABASE_URL,
